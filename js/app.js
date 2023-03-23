7655f0a0ac88a557d3e3d1a31b4b6412a3a8672f
Aboubakar <Aboubakardaouda15@gmail.com>
2023-03-23 12:24:04 +0100

    Traitement JS

--- js/app.js
+++ js/app.js
@@ -4,4 +4,20 @@ const inputTask = document.forms['frmTODO'].task;
 const btAdd = document.forms['frmTODO'].btAdd;
 const taskList = document.getElementById('taskList');
 
-//
+//console.log(inputTask);
+
+//Traitement des actions
+btAdd.onclick = function() {
+	//Lecture des données
+	let task = inputTask.value;
+	
+	//Validation des données
+	if(task!= '' && task.length>=3) {
+		divNotification.innerHTML = '';
+		//Traitement des données
+		
+	} else {
+		//Notification des erreurs
+		divNotification.innerHTML = 'Veuillez entrer une tâche d\'au moins 3 caractères';
+	}
+};
